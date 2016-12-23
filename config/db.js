3895d25a0979e26b0add1d15c36b11af3f7a5227
shijinraj <shijinraj@gmail.com>
2016-12-23 17:19:14 +0530

    mongo db url updated

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-	urlSubjectViews: 'mongodb://admin:SFPWTJELCIHBQGUR@sl-us-dal-9-portal.4.dblayer.com:17886/admin',
+	urlSubjectViews: 'mongodb://7b9adfa9-90dd-4a0a-8aa0-a5824dfb8f7c:1ffa55c5-47bb-4ae3-adb4-dde653afd6cf@75.126.37.68:10014/db',
 	
 
 }
